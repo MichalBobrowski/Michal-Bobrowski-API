cf64939d8d9af646f1d1e0fe9e20c67d6495df0b
“Michal <bobrowski92@gmail.com>
2017-10-18 10:00:43 +0200

    Zadanie 15.2

---
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -0,0 +1,16 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskDto {
+    private Long id;
+    private String title;
+    private String content;
+
+
+}
