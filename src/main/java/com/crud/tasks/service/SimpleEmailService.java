1bb861149c276f147666a3c93875c59c23771d1f
“Michal <bobrowski92@gmail.com>
2017-11-26 11:12:36 +0100

    Test

---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,12 @@
+package com.crud.tasks.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SimpleEmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+}
